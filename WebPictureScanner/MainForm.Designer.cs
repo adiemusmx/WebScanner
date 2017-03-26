6bccdc37609eeda75f9a9f16066fd7d942bf55c9
Leon <adiemusmx@aliyun.com>
2017-03-26 18:32:57 +0800

    更新

--- WebPictureScanner/MainForm.Designer.cs
+++ WebPictureScanner/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.AuthorInfo = new System.Windows.Forms.LinkLabel();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
@@ -241,6 +242,7 @@
 			this.Controls.Add(this.AuthorInfo);
 			this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
 			this.Text = "WebPictureScanner For jd.com";
 			this.ResumeLayout(false);
